cc84179cb2f2e9c232077be7c68e4ca899c91561
Patacode <pata.codegineer@gmail.com>
2026-03-12 19:52:04 +0100

    feat(vidload): add [unreleased] section to preprocessed changelog

--- bin/vidload/scripts/preprocess-changelog-dryrun.sh
+++ bin/vidload/scripts/preprocess-changelog-dryrun.sh
@@ -8,5 +8,5 @@ readonly C_GEM_VERSION_TOKEN="__GEM_VER__"
 readonly C_CURRENT_DATE_TOKEN="__CUR_DT__"
 
 org_value="## \[unreleased]"
-new_value="## [$C_GEM_VERSION_TOKEN] - $C_CURRENT_DATE_TOKEN"
+new_value="## [unreleased]\n\n## [$C_GEM_VERSION_TOKEN] - $C_CURRENT_DATE_TOKEN"
 sed -e "s/$org_value/$new_value/g" $A_CHANGELOG_FILEPATH
