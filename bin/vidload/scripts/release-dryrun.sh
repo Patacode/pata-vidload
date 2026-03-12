6d96753a71a0b45abddd8a0749e239904607a26b
Patacode <pata.codegineer@gmail.com>
2026-03-12 18:44:16 +0100

    feat(vidload): remove tmp changelog file in dryrun release script

--- bin/vidload/scripts/release-dryrun.sh
+++ bin/vidload/scripts/release-dryrun.sh
@@ -18,3 +18,4 @@ clean_exit() {
 ./scripts/preprocess-changelog-dryrun.sh CHANGELOG.md >CHANGELOG-tmp.md || clean_exit 1
 ./scripts/replace-release-tokens-dryrun.sh $A_BUMP_LEVEL CHANGELOG-tmp.md || clean_exit 1
 gem bump -t -r -v "$A_BUMP_LEVEL" --pretend || clean_exit 1
+clean_exit 0
