4ff46f5dcf373110605e810648d93fb533d85ef0
Patacode <pata.codegineer@gmail.com>
2026-03-12 17:28:15 +0100

    refactor(vidload): bump with tagging and releasing in one command

--- bin/vidload/release-dryrun.sh
+++ bin/vidload/release-dryrun.sh
@@ -4,6 +4,4 @@
 readonly A_BUMP_LEVEL="$1"
 
 ./replace-release-tokens-dryrun.sh $A_BUMP_LEVEL CHANGELOG.md
-gem bump "$A_BUMP_LEVEL" --pretend
-gem tag --pretend
-gem release --pretend
+gem bump -t -r -v "$A_BUMP_LEVEL" --pretend
