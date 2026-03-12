e3bc86cc2deea060a782659adb7631708eecf271
Patacode <pata.codegineer@gmail.com>
2026-03-12 20:00:18 +0100

    feat(vidload): add missing git push in release.sh

--- bin/vidload/scripts/release.sh
+++ bin/vidload/scripts/release.sh
@@ -13,3 +13,4 @@ git commit -am "chore: preprocess files before release"
 ./scripts/replace-release-tokens.sh $A_BUMP_LEVEL CHANGELOG.md || exit 1
 git commit -am "chore: replace release tokens"
 gem bump -t -r -v "$A_BUMP_LEVEL" || exit 1
+git push
