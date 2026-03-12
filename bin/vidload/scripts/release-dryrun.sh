c4cbbc3d2c84f78bf1e5bcf9910560dde35e032d
Patacode <pata.codegineer@gmail.com>
2026-03-12 20:32:00 +0100

    fix(release-dryrun): make exit status consistent

--- bin/vidload/scripts/release-dryrun.sh
+++ bin/vidload/scripts/release-dryrun.sh
@@ -12,7 +12,7 @@ readonly A_BUMP_LEVEL="$1"
 clean_exit() {
     local status="$1"
     rm CHANGELOG-tmp.md
-    exit 1
+    exit $status
 }
 
 ./scripts/preprocess-changelog-dryrun.sh CHANGELOG.md >CHANGELOG-tmp.md || clean_exit 1
