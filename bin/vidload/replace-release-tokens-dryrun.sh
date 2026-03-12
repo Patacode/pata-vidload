eacaaeeb69e8256fb105e80f4b584c7977a08ea5
Patacode <pata.codegineer@gmail.com>
2026-03-12 16:44:29 +0100

    feat(vidload): improve ansi color to dryrun output of replace-release-tokens-dryrun.sh

--- bin/vidload/replace-release-tokens-dryrun.sh
+++ bin/vidload/replace-release-tokens-dryrun.sh
@@ -8,12 +8,13 @@ readonly C_NEXT_VERSION="$(./get-next-release-version.sh $A_BUMP_LEVEL)"
 readonly C_CURRENT_DATE="$(date +%d/%m/%Y)"
 readonly C_ANSI_BOLD_LIGHT_GREEN="\033[1;92m"
 readonly C_ANSI_BOLD_LIGHT_RED="\033[1;91m"
+readonly C_ANSI_BOLD_WHITE="\033[1;97m"
 readonly C_ANSI_RESET="\033[0m"
 
 # functions
 print_custom_txt() {
     local txt="$1"
-    echo -n "------------------------------ "
+    printf "$C_ANSI_BOLD_WHITE===========================$C_ANSI_RESET->> "
     printf "$txt\n"
 }
 
@@ -32,14 +33,30 @@ replace_release_tokens() {
     replace_token CUR_DT "$C_CURRENT_DATE" "$file"
 }
 
+print_header() {
+    local file="$1"
+    print_custom_txt "Replacement in $C_ANSI_BOLD_WHITE$file$C_ANSI_RESET file"
+}
+
+print_before() {
+    local file="$1"
+    printf "$C_ANSI_BOLD_LIGHT_RED<<<<<<<<<< Before$C_ANSI_RESET\n"
+    cat "$file"
+}
+
+print_after() {
+    local file="$1"
+    printf "$C_ANSI_BOLD_LIGHT_GREEN>>>>>>>>>> After$C_ANSI_RESET\n"
+    cat "$file"
+}
+
 for file in "${@:2}"; do
-    cat $file >".tmp"
-    print_custom_txt "Replacement in $file file"
-    echo "$C_ANSI_BOLD_LIGHT_RED>>>>>>>>>> Before$C_ANSI_RESET"
-    cat .tmp
+    cat "$file" >.tmp
+    print_header "$file"
+
+    print_before .tmp
     replace_release_tokens .tmp
-    echo "$C_ANSI_BOLD_LIGHT_GREEN<<<<<<<<<< After$C_ANSI_RESET"
-    cat .tmp
+    print_after .tmp
 done
 
 rm -f .tmp
