7b986ad8bee5421e64faa9f75172414de0e8c56a
Patacode <pata.codegineer@gmail.com>
2026-03-12 17:54:33 +0100

    fix(vidload): refer to custom scripts with proper path

--- bin/vidload/scripts/release-dryrun.sh
+++ bin/vidload/scripts/release-dryrun.sh
@@ -3,5 +3,5 @@
 # args
 readonly A_BUMP_LEVEL="$1"
 
-./replace-release-tokens-dryrun.sh $A_BUMP_LEVEL CHANGELOG.md || exit 1
+./scripts/replace-release-tokens-dryrun.sh $A_BUMP_LEVEL CHANGELOG.md || exit 1
 gem bump -t -r -v "$A_BUMP_LEVEL" --pretend || exit 1
